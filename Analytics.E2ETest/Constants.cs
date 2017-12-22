03c06627e2c87932cb68a84f9e1f38e1e3667a51
Kevin Gilliard <kevin.gilliard8415@gmail.com>
2017-12-21 23:09:08 -0800

    Add project files.

---
+++ Analytics.E2ETest/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Segment.E2ETest
+{
+	public class Constants
+	{
+		// project segmentio/dotnet-test
+		public static string WRITE_KEY = "wZqHyttfRO0KxEHyRTujWZQswgTDZx1N";
+
+        public static string RUNSCOPE_TOKEN = "41c14d42-85e7-4bf6-9c9e-2536a374331f";
+	}
+}
+
